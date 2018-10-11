dbf4d3aefac781fee43a9014803bb44db43d95e5
EmilyQiRabbit <LiuYQEmily@163.com>
2018-10-11 14:41:01 +0800

    feat: ejs demo

---
+++ EJSDemo/README.md
@@ -0,0 +1,36 @@
+# EJS Demo
+
+## 相关依赖
+
+```json
+"dependencies": {
+    "@types/ejs": "^2.6.0",
+    "@types/express": "^4.16.0",
+    "@types/fs-extra": "^5.0.4",
+    "ejs": "^2.6.1",
+    "express": "^4.16.3",
+    "fs-extra": "^7.0.0",
+    "tslib": "^1.9.3",
+    "config": "^2.0.1"
+  }
+```
+
+其中：
+
+express: Node 服务框架
+
+ejs: JS 模版
+
+fs-extra: 读取文件
+
+config: 用来做系统配置
+
+## Test step
+
+```sh
+npm i
+
+npm run build
+
+npm start
+```
---
+++ EJSDemo/src/cli.ts
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+import fs from 'fs-extra'
+import { compile } from 'ejs'
+import e from 'express'
+
+// 读取配置
+const config: any = require('config')
+const options = config.default || config
+
+console.log(options)
+
+const { listenPort, serviceTemplateFile } = options
+
+let serviceTemplate: any
+
+const init = () => {
+  return fs.readFile(serviceTemplateFile, 'utf8')
+    .then((content) => {
+      serviceTemplate = compile(content);
+    }).then(() => {
+      listen()
+    });
+}
+
+const renderService = () => {
+  const data = {
+    msg: 'hello EJS'
+  };
+  return serviceTemplate({ data });
+}
+
+const listen = () => {
+  return new Promise((resolve) => {
+    app.listen(listenPort, '0.0.0.0', () => {
+      console.info('http server listening', { port: listenPort });
+      resolve();
+    });
+  });
+}
+
+// main
+const app = e();
+init()
+
+app.get('*', (req, res) => {
+  res.end(renderService());
+});
